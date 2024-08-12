299b0261a56fab8fc85d0a384e64c27b5cd99a53
Gaurav Mall <45404510+GauravMall@users.noreply.github.com>
2024-08-12 12:54:51 +0300

    Updated functionality

--- OpenJourn Site/script.js
+++ OpenJourn Site/script.js
@@ -1,4 +1,3 @@
-
 const dropArea = document.getElementById("drag-drop-area");
 const inputFile = document.getElementById("input-file");
 const imgView = document.getElementById("img-view");
@@ -6,27 +5,38 @@ const imgContaienr = document.getElementById("img-container");
 
 const queryInput = document.querySelector("textarea");
 
-const fileName = document.getElementById("file-name")
+const fileName = document.getElementById("file-name");
+
+const generateBtn = document.getElementById("generate-btn");
 
 inputFile.addEventListener("change", changePDFStatus);
 
-function changePDFStatus() {
-    imgView.src = "images/file_pdf.png";
-    fileName.innerText = inputFile.files[0].name;
-}
-
-dropArea.addEventListener("dragover", e => {
+dropArea.addEventListener("dragover", (e) => {
   e.preventDefault();
 });
 
-dropArea.addEventListener("drop", e => {
+dropArea.addEventListener("drop", (e) => {
   e.preventDefault();
   inputFile.files = e.dataTransfer.files;
   changePDFStatus();
-})
+});
 
-queryInput.addEventListener("input", e => {
+function changePDFStatus() {
+  imgView.src = "images/file_pdf.png";
+  fileName.innerText = inputFile.files[0].name;
+}
+
+queryInput.addEventListener("input", (e) => {
   queryInput.style.height = "auto";
   let scHeight = e.target.scrollHeight;
   queryInput.style.height = `${scHeight}px`;
-})
+});
+
+generateBtn.addEventListener("click", (e) => {
+  var query = queryInput.value;
+  var file = inputFile.files[0];
+
+  if (file != undefined) {
+    //Send data to AI
+  }
+});
